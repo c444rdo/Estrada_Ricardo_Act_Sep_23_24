8883b9ef0015fe58a44f82894ab6b7305c53a8b7
Ricardo Martínez <a.23308051281193@cbtis128.edu.mx>
2024-09-23 20:21:03 -0600

    Add files via upload

---
+++ CONVERSIÓN_C-F.py
@@ -0,0 +1,11 @@
+print("Ricardo Martínez Estrada")
+print(" ")
+
+# Solicitar al usuario la temperatura en Fahrenheit.
+fahrenheit = float(input("Ingrese la temperatura en grados Fahrenheit: "))
+
+# Calcular la temperatura en Celsius.
+celsius = (5 / 9) * (fahrenheit - 32)
+
+# Mostrar el resultado.
+print("El equivalente en grados Celsius es:", celsius)
---
+++ MULT_3VAL.py
@@ -0,0 +1,11 @@
+print ("Ricardo Martínez Estrada")
+print (" ")
+
+# Código donde se solicitan 3 valores y se multiplican.
+num1 = float(input("Ingrese el primer valor: "))
+num2 = float(input("Ingrese el segundo valor: "))
+num3 = float(input("Ingrese el tercer valor: "))
+
+# Multiplicar los valores e imprimirlos en pantalla.
+multiplicacion = (num1*num2*num3)
+print ("La multiplicación de los valores es: ", multiplicacion)
